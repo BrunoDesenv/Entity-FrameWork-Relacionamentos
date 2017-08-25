b62a1d258deea4ee6e7c14f879a08b8087b8ce45
Bruno Santos <brunoj.santos91@gmail.com>
2017-08-25 00:02:59 -0300

    Criação do projeto

---
+++ UmParaUm/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmParaUm
+{
+    public class Contexto
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entidades.Cliente>()
+                .HasKey(x => x.TodoID)
+                .HasRequired(x => x.ObjTodo)
+                .WithRequiredDependent(p => p.ObjParte);
+        }
+    }
+}
---
+++ UmParaUm/Entidades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmParaUm
+{
+    public class Entidades
+    {
+
+        public class Cliente
+        {
+            public int Codigo { get; set; }
+            public string Nome { get; set; }
+            public int CodigoEndereco { get; set; }
+            public virtual Endereco Endereco { get; set; }
+        }
+
+        public class Endereco
+        {
+            public int Codigo { get; set; }
+            public string Descricao { get; set; }
+            public string Numero { get; set; }
+        }
+    }
+}
